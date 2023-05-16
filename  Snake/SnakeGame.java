fc1d8917b7c4047e3bb051b24356a50230dcc463
Titus Bino <102717767+titusbino@users.noreply.github.com>
2023-05-16 14:23:55 +0800

    Add files via upload
    This is a Java program that implements the classic "Snake" game. It uses the Swing graphics library to create a graphical user interface for the game.
    
    The program defines a SnakeGame class that extends the JPanel class and implements the KeyListener and Runnable interfaces. It contains several constants and instance variables:
    
    - WIDTH, HEIGHT, and SCALE define the size of the game window and the size of each block in the game.
    - FPS defines the number of frames per second that the game runs at.
    - RANDOM is an instance of the Java Random class that is used to generate random positions for the food.
    - snake is a LinkedList that stores the positions of the snake's body segments.
    - food is a Point object that stores the position of the food.
    - direction stores the current direction that the snake is moving in.
    - isRunning is a boolean flag that is used to control the game loop.
    
    The constructor for the SnakeGame class sets up the game window by creating a JFrame object and adding the current object (this) as a key listener and a JPanel. It also initializes the snake, food, and direction, and starts a new thread to run the game loop.
    
    The paintComponent method is called by Swing whenever the game window needs to be redrawn. It draws the black background, the red food block, and the green snake blocks using the Graphics object that is passed in as a parameter.
    
    The keyPressed method is called by Swing whenever a key is pressed. It updates the direction variable based on the key that was pressed, as long as the new direction is not opposite to the current direction.
    
    The run method is the main game loop. It runs continuously while the is Running flag is set to true. On each iteration of the loop, it updates the position of the snake's head based on the current direction, and checks whether the head has collided with the food or any part of the snake's body. If a collision occurs, the game updates the food position and adds a new segment to the snake's body. If a collision with the wall or the snake's body occurs, the game ends and the is Running flag is set to false. Finally, the method calls repaint to redraw the game window, and sleeps for a short period of time to limit the frame rate.

---
+++  Snake/SnakeGame.java
@@ -0,0 +1,113 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.LinkedList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SnakeGame extends JPanel implements KeyListener, Runnable {
+    
+    private static final long serialVersionUID = 1L;
+
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+    private static final int SCALE = 10;
+    private static final int FPS = 10;
+
+    private static final Random RANDOM = new Random();
+    
+    private LinkedList<Point> snake;
+    private Point food;
+    private int direction;
+    private boolean isRunning;
+
+    public SnakeGame() {
+        snake = new LinkedList<>();
+        snake.add(new Point(0, 0));
+        food = new Point(RANDOM.nextInt(WIDTH / SCALE) * SCALE, RANDOM.nextInt(HEIGHT / SCALE) * SCALE);
+        direction = KeyEvent.VK_RIGHT;
+        isRunning = true;
+        JFrame frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.addKeyListener(this);
+        frame.add(this);
+        frame.setVisible(true);
+        new Thread(this).start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        g.setColor(Color.RED);
+        g.fillRect(food.x, food.y, SCALE, SCALE);
+        g.setColor(Color.GREEN);
+        for (Point p : snake) {
+            g.fillRect(p.x, p.y, SCALE, SCALE);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN) {
+            if (Math.abs(direction - key) != 2) {
+                direction = key;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            try {
+                Thread.sleep(1000 / FPS);
+                Point head = snake.getFirst();
+                int x = head.x;
+                int y = head.y;
+                switch (direction) {
+                    case KeyEvent.VK_LEFT:
+                        x -= SCALE;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        x += SCALE;
+                        break;
+                    case KeyEvent.VK_UP:
+                        y -= SCALE;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        y += SCALE;
+                        break;
+                }
+                Point nextHead = new Point(x, y);
+                if (nextHead.equals(food)) {
+                    food = new Point(RANDOM.nextInt(WIDTH / SCALE) * SCALE, RANDOM.nextInt(HEIGHT / SCALE) * SCALE);
+                } else {
+                    snake.removeLast();
+                }
+                if (snake.contains(nextHead) || x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+                    isRunning = false;
+                } else {
+                    snake.addFirst(nextHead);
+                    repaint();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Game Over!");
+    }
+    
+
